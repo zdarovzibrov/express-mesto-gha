d4a40da17bad5939f24aa9e646cf74147c5c0c67
Mik Zibrov <zdarovzibrov@yandex.ru>
2023-04-27 23:50:18 +0300

    proverka PR 14 sprint autotest

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post(
   '/signup',
   celebrate({
-    body: Joi.object()
-      .keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-      })
-      .unknown(true),
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(urlRegEx),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }).unknown(true),
   }),
   createUser
 );
@@ -30,12 +31,10 @@ app.post(
 app.post(
   '/signin',
   celebrate({
-    body: Joi.object()
-      .keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-      })
-      .unknown(true),
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }).unknown(true),
   }),
   login
 );
