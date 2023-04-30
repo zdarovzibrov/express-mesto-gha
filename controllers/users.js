d5859999621c184734a81508b2fcb1674b72752d
Mik Zibrov <zdarovzibrov@yandex.ru>
2023-04-30 23:41:33 +0300

    proverka

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const UnauthorizedError = require('../errors/unauthorized');
 const NotFoundError = require('../errors/notfound');
 const ConflictError = require('../errors/conflict');
+const BadRequestError = require('../errors/badrequest');
 
 const getAllUsers = (req, res, next) => {
   User.find({})
@@ -32,10 +33,19 @@ const createUser = (req, res, next) => {
       }))
       .catch((err) => {
         if (err.code === 11000) {
-          next(new ConflictError('Пользователь с таким email уже существует.'));
+          next(
+            new ConflictError('Пользователь с таким email уже существует.')
+          );
+        } else if (err.name === 'ValidationError') {
+          next(
+            new BadRequestError('Некорректные данные при создании пользователя.')
+          );
+        } else {
+          next(err);
         }
       });
-  });
+  })
+    .catch(next);
 };
 
 const login = (req, res, next) => {
