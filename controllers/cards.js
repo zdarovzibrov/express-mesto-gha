1c95083422bb6865cc7cdf93a95bee7ec408abd5
Mik Zibrov <zdarovzibrov@yandex.ru>
2023-04-30 23:11:49 +0300

    fix: unknown(true), 404_to_next, link_validator, next_err_400, add_else_next_err, block_catch, udalenie v dva etapa, proverka_body, id_valid_hex

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,23 +32,17 @@ const createCard = (req, res, next) => {
 };
 
 const deleteCard = (req, res, next) => {
-  Card.findById(req.params.cardId)
-    .then((reqCard) => {
-      if (!reqCard) {
-        throw new NotFoundError('Некорректный Id');
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw new NotFoundError('Информация по карточке не найдена.');
       }
-      if (reqCard.owner.toString() === req.user._id) {
-        Card.findByIdAndRemove(req.params.cardId)
-          .then((card) => {
-            res.send({ data: card });
-          });
-      } else {
-        next(new ForbiddenError('Это не ваша карточка'));
+      if (card.owner.toString() !== req.user._id) {
+        throw new ForbiddenError('Нет прав на удаление.');
       }
+      return res.send(card);
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 };
 
 const likeCard = (req, res, next) => {
